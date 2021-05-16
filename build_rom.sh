063ffe4cce18e9f0aa8dbf828470cc6012d853e4
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-16 22:58:50 +0530

    here we go again

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ set -x
 # sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync
 
 # build rom
 source build/envsetup.sh
